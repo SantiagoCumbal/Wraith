17d62e978c4f44515169c9ecaa922ded768d2e25
SantiagoCumbal <151807183+SantiagoCumbal@users.noreply.github.com>
2025-08-04 00:37:14 -0500

    arreglo google

--- Backend/src/config/passport.js
+++ Backend/src/config/passport.js
@@ -23,7 +23,7 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback',
+  callbackURL: 'https://wraith-24zv.onrender.com/auth/google/callback',
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     let user = await Jugadores.findOne({ avatarJugadorID: profile.id });
