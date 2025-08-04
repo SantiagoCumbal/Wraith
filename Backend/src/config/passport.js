04d8bab26b5ca9435e8b0f4c81e3efc2990d4ba1
SantiagoCumbal <151807183+SantiagoCumbal@users.noreply.github.com>
2025-08-04 01:09:38 -0500

    proceso facebook

--- Backend/src/config/passport.js
+++ Backend/src/config/passport.js
@@ -53,7 +53,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
   clientID: process.env.FACEBOOK_APP_ID,
   clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: '/auth/facebook/callback',
+  callbackURL: 'https://wraith-24zv.onrender.com/auth/facebook/callback',
   profileFields: ['id', 'displayName', 'email', 'name', 'photos'],
 }, async (accessToken, refreshToken, profile, done) => {
   try {
