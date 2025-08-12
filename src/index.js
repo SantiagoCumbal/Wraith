2e8698e438fda2f21d41e1c5161599151371dcff
SantiagoCumbal <151807183+SantiagoCumbal@users.noreply.github.com>
2025-08-12 02:39:27 -0500

    Delete src directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import {app, server }from './server.js'
-import connection from './database.js'
-
-connection()
-
-server.listen(app.get('port'),()=>{
-    console.log(`Server ok on http://localhost:${app.get('port')}`);
-})
